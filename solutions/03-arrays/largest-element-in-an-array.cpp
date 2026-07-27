da0e7f4365d25a5ca9956d97e054de5a9d7419bb
Ojas Kulkarni <149525656+ojasra0kulkarni@users.noreply.github.com>
2026-07-27 19:45:54 +0530

    Add largest element in an array

--- solutions/03-arrays/largest-element-in-an-array.cpp
+++ solutions/03-arrays/largest-element-in-an-array.cpp
@@ -1,27 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-#define pb push_back
-
-// Largest Element in an Array
-int largestElement(vector<int> &arr) {
-    // tricky part: assume array is not empty for this problem
-    int maxVal = arr[0];
-    for (int i = 1; i < arr.size(); ++i) {
-        if (arr[i] > maxVal) {
-            maxVal = arr[i];
+class Solution {
+public:
+    int largestElement(vector<int> &arr) {
+        int n = arr.size();
+        int mx = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > mx) {
+                mx = arr[i];
+            }
         }
+        return mx;
     }
-    return maxVal;
-}
-
-int main() {
-    vector<int> nums = {3, 2, 1, 5, 2};
-    cout << "Largest element is: " << largestElement(nums) << endl; // Expect 5
-    vector<int> single = {7};
-    cout << "Largest element is: " << largestElement(single) << endl; // Expect 7
-    vector<int> negs = {-10, -5, -20};
-    cout << "Largest element is: " << largestElement(negs) << endl; // Expect -5
-    return 0;
-}
+};
