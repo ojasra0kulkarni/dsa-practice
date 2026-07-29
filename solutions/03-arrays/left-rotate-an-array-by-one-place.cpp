9d0a4186c86af0f3cf6fa40c8445d4efd95ce7ee
Ojas Kulkarni <149525656+ojasra0kulkarni@users.noreply.github.com>
2026-07-29 21:00:20 +0530

    Left Rotate an array by one place | O(n)

---
+++ solutions/03-arrays/left-rotate-an-array-by-one-place.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+
+class Solution {
+public:
+    void leftRotate(vector<int> &arr) {
+        int n = arr.size();
+        if (n <= 1) return; 
+        int tmp = arr[0];
+        for(int i=0;i<n-1;i++){
+            arr[i] = arr[i+1];
+        }
+        arr[n-1] = tmp;
+    }
+};
