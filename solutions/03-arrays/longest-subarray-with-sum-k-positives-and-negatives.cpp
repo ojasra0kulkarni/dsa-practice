6d5a169c9af189001a5f5f87176deabec09d22b7
Ojas Kulkarni <149525656+ojasra0kulkarni@users.noreply.github.com>
2026-08-03 16:38:39 +0530

    arrays: Longest subarray with sum K (positives and negatives)

---
+++ solutions/03-arrays/longest-subarray-with-sum-k-positives-and-negatives.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestSubarrayWithSumK(vector<int> a, int k) {
+        unordered_map<long long, int> mp; 
+        long long sum = 0;
+        int mx = 0;
+        mp[0] = -1; // sum 0 found at index -1
+
+        for(int i=0;i<a.size();i++){
+            sum += a[i];
+            if(mp.count(sum - k)){
+                mx = max(mx, i - mp[sum - k]);
+            }
+            if(!mp.count(sum)){ // store first occurrence
+                mp[sum] = i;
+            }
+        }
+        return mx;
+    }
+};
