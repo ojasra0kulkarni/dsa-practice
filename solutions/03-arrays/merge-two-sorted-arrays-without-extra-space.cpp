5439fc67af99efeb1c1485f626679bad17fd029e
Ojas Kulkarni <149525656+ojasra0kulkarni@users.noreply.github.com>
2026-08-09 18:59:45 +0530

    merge two sorted arrays without extra space done

---
+++ solutions/03-arrays/merge-two-sorted-arrays-without-extra-space.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& arr1, vector<int>& arr2, int n, int m) {
+        int i = n - 1;
+        int j = 0;
+
+        while (i >= 0 && j < m) {
+            if (arr1[i] > arr2[j]) {
+                swap(arr1[i], arr2[j]);
+                i--;
+                j++;
+            } else {
+                break; // elements are already in correct relative positions
+            }
+        }
+        sort(arr1.begin(), arr1.end());
+        sort(arr2.begin(), arr2.end());
+    }
+};
