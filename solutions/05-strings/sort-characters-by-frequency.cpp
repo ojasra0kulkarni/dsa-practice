1499b48f96e0c4a3f620dd387b34944a0b4c67b2
Ojas Kulkarni <149525656+ojasra0kulkarni@users.noreply.github.com>
2026-08-31 16:21:14 +0530

    solved sort characters by frequency

---
+++ solutions/05-strings/sort-characters-by-frequency.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        map<char, int> mp;
+        for (char c : s) {
+            mp[c]++;
+        }
+
+        priority_queue<pair<int, char>> pq;
+        for (auto it : mp) {
+            pq.push({it.second, it.first});
+        }
+
+        string ans = "";
+        while (!pq.empty()) {
+            pair<int, char> curr = pq.top();
+            pq.pop();
+            int freq = curr.first;
+            char c = curr.second;
+            for (int i = 0; i < freq; i++) {
+                ans += c;
+            }
+        }
+        return ans;
+    }
+};
