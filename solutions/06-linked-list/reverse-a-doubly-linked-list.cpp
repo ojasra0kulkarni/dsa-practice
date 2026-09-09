4b30c2d513f4df1945c01ed399787de5a59ba7f7
Ojas Kulkarni <149525656+ojasra0kulkarni@users.noreply.github.com>
2026-09-09 22:21:08 +0530

    reverse a doubly linked list

---
+++ solutions/06-linked-list/reverse-a-doubly-linked-list.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *prev;
+    Node *next;
+    Node() : data(0), prev(nullptr), next(nullptr) {}
+    Node(int x) : data(x), prev(nullptr), next(nullptr) {}
+    Node(int x, Node *p, Node *n) : data(x), prev(p), next(n) {}
+};
+
+Node* reverseDLL(Node *head) {
+    if (head == nullptr || head->next == nullptr) {
+        return head;
+    }
+
+    Node* curr = head;
+    Node* p = nullptr;
+    Node* nxt = nullptr;
+
+    while (curr != nullptr) {
+        nxt = curr->next;
+
+        curr->next = p;
+        curr->prev = nxt;
+
+        p = curr;
+        curr = nxt;
+    }
+
+    return p;
+    
+}
