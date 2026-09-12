45a47b60f5386d25a0d08a03366392b6e7a1af27
Ojas Kulkarni <149525656+ojasra0kulkarni@users.noreply.github.com>
2026-09-12 16:52:02 +0530

    day 48 - remove nth node from the back of the linked list

---
+++ solutions/06-linked-list/remove-nth-node-from-the-back-of-the-linked-list.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* res = new ListNode(0);
+        res->next = head;
+        ListNode* fast = res;
+        ListNode* slow = res;
+
+        for(int i=0;i<n;i++) {
+            fast = fast->next;
+        }
+
+        while(fast->next!=nullptr) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        slow->next = slow->next->next; // skip the nth node
+
+        return res->next;
+    }
+    
+};
