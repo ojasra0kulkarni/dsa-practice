0119f8f8b4f67613f75fefeeed720c502b1e7298
Ojas Kulkarni <149525656+ojasra0kulkarni@users.noreply.github.com>
2026-09-14 21:54:20 +0530

    Delete all occurrences of a key in Doubly Linked List | O(N)

---
+++ solutions/06-linked-list/delete-all-occurrences-of-a-key-in-doubly-linked-list.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+    Node *prev;
+};
+
+class Solution {
+public:
+    Node* deleteAllOccurrences(Node* head, int k) {
+        Node* curr = head;
+        while (curr != nullptr) {
+            if (curr->data == k) {
+                Node* tmp = curr->next;
+
+                if (curr->prev != nullptr) {
+                    curr->prev->next = curr->next;
+                } else {
+                    head = curr->next;
+                }
+
+                if (curr->next != nullptr) {
+                    curr->next->prev = curr->prev;
+                }
+                
+                delete curr;
+                curr = tmp;
+            } else {
+                curr = curr->next;
+            }
+        }
+        return head;
+    }
+};
